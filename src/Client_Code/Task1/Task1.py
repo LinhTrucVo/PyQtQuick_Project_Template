a1f253c4b641a13b7b8a7b1949de534944e241e1
vor5hc <Truc.VoLinh@vn.bosch.com>
2025-12-22 11:20:54 +0700

    update message handling by dict instead of if-else, this make the code cleaner

--- src/Client_Code/Task1/Task1.py
+++ src/Client_Code/Task1/Task1.py
@@ -20,6 +20,42 @@ class Task1(Bico_QUIThread):
     count = 0  # Static count for thread naming
     ex_data_obj = Task1_Data()
 
+    def __init__(self, qin=None, qin_owner=0, qout=None, qout_owner=0, obj_name="", ui_path="", parent=None):
+        """
+        Initialize Task1 thread with message handlers.
+        
+        Parameters
+        ----------
+        qin : Bico_QMutexQueue, optional
+            Input queue for the thread.
+        qin_owner : int, optional
+            Ownership flag for input queue.
+        qout : Bico_QMutexQueue, optional
+            Output queue for the thread.
+        qout_owner : int, optional
+            Ownership flag for output queue.
+        obj_name : str
+            Name/identifier for the thread object.
+        ui_path : str, optional
+            Path to the QML UI file.
+        parent : QObject, optional
+            Parent QObject for parent-child relationship.
+        """
+        # Call parent class __init__
+        super().__init__(qin, qin_owner, qout, qout_owner, obj_name, ui_path, parent)
+        
+        # Message handler dictionary - cleaner than if-else chains
+        self.message_handlers = {
+            "terminate": self._handle_terminate,
+            "num1": self._handle_num1,
+            "num2": self._handle_num2,
+            "text": self._handle_text,
+            "create": self._handle_create,
+            "create_child": self._handle_create_child,
+            "size": self._handle_size,
+            "from_another_thread": self._handle_from_another_thread,
+        }
+
     def cleanupChildren(self):
         """
         Cleanup child threads before this thread is destroyed.
@@ -49,57 +85,16 @@ class Task1(Bico_QUIThread):
         """
         continue_to_run = 1
         
-        i = 0
         input, result = self.qinDequeue()
 
         if result:
             mess = input.mess()
             data = input.data()
-            if (mess == "terminate"):            
-                continue_to_run = 0
-                self.cleanupChildren()
-            elif (mess == "num1"):
-                print(self.objectName() + " " + mess + " " + str(self.ex_data_obj.getData_1()))
-            elif (mess == "num2"):
-                print(self.objectName() + " " + mess + " " + str(self.ex_data_obj.getData_2()))
-            elif (mess == "text"):
-                print(self.objectName() + " " + mess + " " + data)
-            elif (mess == "create"):
-                print(self.objectName() + " " + mess + " " + data)
-                Task1.count += 1
-                thread_name = "task_" + str(Task1.count)
-                Bico_QUIThread.create(
-                    # Using qml which is intergrated to Qt resource
-                    Task1,
-                    Bico_QMutexQueue(),
-                    1, 
-                    Bico_QMutexQueue(), 
-                    1, 
-                    thread_name, 
-                    "qrc:/Client_Code/Task1/UI/Task1Content/App.qml"
-                )
-                Bico_QUIThread.getThreadHash()[thread_name].start()
-            elif (mess == "create_child"):
-                print(self.objectName() + " " + mess + " " + data)
-                Task1.count += 1
-                thread_name = "task_" + str(Task1.count)
-                Bico_QUIThread.create(
-                    # Using qml which is intergrated to Qt resource
-                    Task1,
-                    Bico_QMutexQueue(),
-                    1, 
-                    Bico_QMutexQueue(), 
-                    1, 
-                    thread_name, 
-                    "qrc:/Client_Code/Task1/UI/Task1Content/App.qml",
-                    self # set parent
-                )
-                Bico_QUIThread.getThreadHash()[thread_name].start()
-            elif (mess == "size"):
-                print(self.objectName() + " " + mess + " " + str(data.width()) + str(data.height()))
-                self.toUI.emit(mess, data)
-            elif (mess == "from_another_thread"):
-                print(self.objectName() + " " + mess + ": "  + input.src() + " - " + str(data))
+            
+            # Use the message handler dictionary from __init__
+            handler = self.message_handlers.get(mess)
+            if handler:
+                continue_to_run = handler(data, input)
 
         print("Hello from " + self.objectName())
         print("Num of running thread: " + str(len(Bico_QUIThread.getThreadHash())))
@@ -110,6 +105,73 @@ class Task1(Bico_QUIThread):
             mess_data = Bico_QMessData("from_another_thread", self.i)
             mess_data.setSrc(self.objectName())
             Bico_QUIThread.getThreadHash().get("task_0").qinEnqueue(mess_data)
-            # self.msleep(2365)
 
         return continue_to_run
+
+    def _handle_terminate(self, data, input_msg):
+        """Handle terminate message."""
+        self.cleanupChildren()
+        return 0  # Signal to stop running
+
+    def _handle_num1(self, data, input_msg):
+        """Handle num1 message."""
+        print(self.objectName() + " num1 " + str(self.ex_data_obj.getData_1()))
+        return 1
+
+    def _handle_num2(self, data, input_msg):
+        """Handle num2 message."""
+        print(self.objectName() + " num2 " + str(self.ex_data_obj.getData_2()))
+        return 1
+
+    def _handle_text(self, data, input_msg):
+        """Handle text message."""
+        print(self.objectName() + " text " + data)
+        return 1
+
+    def _handle_create(self, data, input_msg):
+        """Handle create message to create sibling thread."""
+        print(self.objectName() + " create " + data)
+        Task1.count += 1
+        thread_name = "task_" + str(Task1.count)
+        thread = Bico_QUIThread.create(
+            Task1,
+            Bico_QMutexQueue(),
+            1, 
+            Bico_QMutexQueue(), 
+            1, 
+            thread_name, 
+            "qrc:/Client_Code/Task1/UI/Task1Content/App.qml"
+        )
+        if thread:
+            thread.start()
+        return 1
+
+    def _handle_create_child(self, data, input_msg):
+        """Handle create_child message to create child thread."""
+        print(self.objectName() + " create_child " + data)
+        Task1.count += 1
+        thread_name = "task_" + str(Task1.count)
+        thread = Bico_QUIThread.create(
+            Task1,
+            Bico_QMutexQueue(),
+            1, 
+            Bico_QMutexQueue(), 
+            1, 
+            thread_name, 
+            "qrc:/Client_Code/Task1/UI/Task1Content/App.qml",
+            self  # set parent
+        )
+        if thread:
+            thread.start()
+        return 1
+
+    def _handle_size(self, data, input_msg):
+        """Handle size message."""
+        print(self.objectName() + " size " + str(data.width()) + str(data.height()))
+        self.toUI.emit("size", data)
+        return 1
+
+    def _handle_from_another_thread(self, data, input_msg):
+        """Handle from_another_thread message."""
+        print(self.objectName() + " from_another_thread: " + input_msg.src() + " - " + str(data))
+        return 1
